3578ed2663fd5b07c8a5857430c0f6b2ea1fee24
Aswinkumar <aswinkumar.cm@entri.me>
2024-09-04 18:17:54 +0530

    ui update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
 
 export const Home = () => {
     return (
         <div className="">
-            <main className="min-h-96 flex gap-10 px-20">
-                <div className="flex-1">
-                    <h1 className="font-bold text-5xl my-5  ">Welcome user, </h1>
-                    <p className="text-2xl font-normal">
+            <main className="min-h-96 flex gap-20 px-20 py-10 w-full">
+                <div className="w-8/12">
+                    <h1 className="font-bold text-4xl my-5  ">Welcome user, </h1>
+                    <p className="text-xl font-normal">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis aspernatur ratione sint odit minus, saepe distinctio
                         cupiditate necessitatibus nemo, laboriosam perspiciatis vero blanditiis, ut corrupti!
                     </p>
                 </div>
-                <div className="flex-1">
-                    <img src="https://archive.org/download/placeholder-image/placeholder-image.jpg" alt="home-image" />
+                <div className="w-5/12">
+                    <img className="w-full" src="https://archive.org/download/placeholder-image/placeholder-image.jpg" alt="home-image" />
                 </div>
             </main>
         </div>
