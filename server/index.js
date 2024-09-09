96f7dd5a14f03ad8c7d39cd37be51ffef1a6879e
Aswinkumar <aswinkumar.cm@entri.me>
2024-09-09 20:48:03 +0530

    vercel.json added

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5100",
+        origin: ["http://localhost:5100", "https://elearning-app-phi.vercel.app"],
         credentials: true,
     })
 );
